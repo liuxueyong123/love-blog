4c856e3cf880c0f96d3ca663529f0b22be661183
lxy <liuxueyong@agora.io>
2021-02-13 23:29:38 +0800

    a

--- website/oss.js
+++ website/oss.js
@@ -1,42 +1,37 @@
 const OSS = require('ali-oss');
 const fs = require('fs');
+const config = require('./config');
 
-const cssPath = './dist/css/'
-const jsPath = './dist/js/'
+const cssPath = './dist/css/';
+const jsPath = './dist/js/';
 
-const client = new OSS({
-  region: 'oss-cn-shanghai',
-  accessKeyId: 'LTAI4G1FJ9dkZpxbcEGseh5L',
-  accessKeySecret: 'PkTNQqfefhXu1beZqoUkvRX0Sb1oZz'
-});
+const client = new OSS(config.oss);
 
-client.useBucket('lxyaitsy')
+client.useBucket('lxyaitsy');
 
-const cssFileList = fs.readdirSync(cssPath)
-const jsFileList = fs.readdirSync(jsPath)
+const cssFileList = fs.readdirSync(cssPath);
+const jsFileList = fs.readdirSync(jsPath);
 
 const uploadFiles = async () => {
-
-  for(const cssFile of cssFileList) {
+  for (const cssFile of cssFileList) {
     try {
-      await client.put(`css/${cssFile}`, `${cssPath}${cssFile}`)
-    } catch(err) {
-      console.log(err)
+      await client.put(`css/${cssFile}`, `${cssPath}${cssFile}`);
+    } catch (err) {
+      console.log(err);
     }
   }
 
-  console.log('css files is uploaded')
+  console.log('css files is uploaded');
 
-  for(const jsFile of jsFileList) {
+  for (const jsFile of jsFileList) {
     try {
-      await client.put(`js/${jsFile}`, `${jsPath}${jsFile}`)
-    } catch(err) {
-      console.log(err)
+      await client.put(`js/${jsFile}`, `${jsPath}${jsFile}`);
+    } catch (err) {
+      console.log(err);
     }
   }
 
-  console.log('js files is uploaded')
-}
+  console.log('js files is uploaded');
+};
 
 uploadFiles();
-     
