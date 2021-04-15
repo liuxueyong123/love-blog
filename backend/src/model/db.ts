dee776d37a62cd1faf2a6026741a7ac6441d949f
lxy <liuxueyong@agora.io>
2021-04-15 11:52:52 +0800

    config.js => config.ts

--- backend/src/model/db.ts
+++ backend/src/model/db.ts
@@ -1,5 +1,4 @@
 import { Sequelize, Options } from 'sequelize'
-const config = require('../../config')
-// import config from '../../config'
+import config from '../../config'
 
 export const loveBlog = new Sequelize(config.mysql as Options)
