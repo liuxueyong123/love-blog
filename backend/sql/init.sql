62f5e7154b193808a07e30483130946a8cf3db87
liuxueyong <liuxueyong@agora.io>
2021-08-02 18:30:14 +0800

    补充 redeme

--- backend/sql/init.sql
+++ backend/sql/init.sql
@@ -1,95 +1,201 @@
-CREATE TABLE `user` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `account` varchar(255)  NOT NULL DEFAULT '',
-  `password` varchar(255)  NOT NULL DEFAULT '',
-  `name` varchar(255)  NOT NULL DEFAULT '',
-  `gender` tinyint  NOT NULL DEFAULT 1 comment '1: female; 2:male',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 5446
+#
+# https://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: rm-uf6s0vx8lzjz7033n9o.mysql.rds.aliyuncs.com (MySQL 8.0.18)
+# Database: love_blog
+# Generation Time: 2021-08-02 10:18:17 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table album
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `album`;
 
 CREATE TABLE `album` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `image_link` varchar(255)  NOT NULL DEFAULT '',
-  `picture_time` varchar(255)  NOT NULL DEFAULT '',
-  `description` varchar(255) DEFAULT '',
-  `type_id` int  NOT NULL,
-  `upload_account_id` int NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `image_link` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `picture_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '',
+  `type_id` int(11) NOT NULL,
+  `upload_account_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table album_type
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `album_type`;
 
 CREATE TABLE `album_type` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `type_name` varchar(255)  NOT NULL DEFAULT '',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `type_detail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `type_icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE `post` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `publisher_id` int  NOT NULL,
-  `content` text  NOT NULL,
-  `publish_time` TIMESTAMP not null,
-  `type_id` int NOT NULL,
+
+
+# Dump of table article
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `article`;
+
+CREATE TABLE `article` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `publisher_id` int(11) NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `publish_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `type_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE `post_type` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `type_name` varchar(255)  NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE `post_like` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `account_id` int  NOT NULL,
-  `post_id` int  NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE `post_comment` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `post_id` int  NOT NULL,
-  `content` text  NOT NULL,
-  `publish_time`  varchar(255)  NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+# Dump of table article_comment
+# ------------------------------------------------------------
 
-CREATE TABLE `article` ( 
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `publisher_id` int  NOT NULL,
-  `content` text  NOT NULL,
-  `publish_time` varchar(255) DEFAULT '',
-  `type_id` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-CREATE TABLE `article_type` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `type_name` varchar(255)  NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-CREATE TABLE `article_like` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `account_id` int  NOT NULL,
-  `article_id` int  NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+DROP TABLE IF EXISTS `article_comment`;
 
 CREATE TABLE `article_comment` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `article_id` int  NOT NULL,
-  `content` text  NOT NULL,
-  `publish_time`  varchar(255)  NOT NULL DEFAULT '',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NOT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `publish_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- 增加类型说明
-ALTER TABLE album_type ADD COLUMN type_detail VARCHAR(255) DEFAULT NULL;
-ALTER TABLE post_type ADD COLUMN type_detail VARCHAR(255) DEFAULT NULL;
-ALTER TABLE article_type ADD COLUMN type_detail VARCHAR(255) DEFAULT NULL;
 
--- 增加类型图标
-ALTER TABLE album_type ADD COLUMN type_icon VARCHAR(255) DEFAULT NULL;
-ALTER TABLE post_type ADD COLUMN type_icon VARCHAR(255) DEFAULT NULL;
-ALTER TABLE article_type ADD COLUMN type_icon VARCHAR(255) DEFAULT NULL;
 
+# Dump of table article_like
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `article_like`;
+
+CREATE TABLE `article_like` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table article_type
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `article_type`;
+
+CREATE TABLE `article_type` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `type_detail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `type_icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table post
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `post`;
+
+CREATE TABLE `post` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `publisher_id` int(11) NOT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `publish_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `type_id` int(11) NOT NULL,
+  `img_list` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table post_comment
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `post_comment`;
+
+CREATE TABLE `post_comment` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `publish_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table post_like
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `post_like`;
+
+CREATE TABLE `post_like` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table post_type
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `post_type`;
+
+CREATE TABLE `post_type` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `type_detail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `type_icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table user
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `account` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `gender` tinyint(4) NOT NULL DEFAULT '1' COMMENT '1: female; 2:male',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
