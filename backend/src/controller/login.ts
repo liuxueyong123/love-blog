903cf093a00f1fcf0ed0664fd5605d9ef4be1a39
liuxueyong <liuxueyong@agora.io>
2021-12-23 17:29:58 +0800

    aa:wq

--- backend/src/controller/login.ts
+++ backend/src/controller/login.ts
@@ -57,7 +57,175 @@ router.route({
     console.log('ctx.request.query', ctx.request.query)
     console.log('ctx.request.body', ctx.request.body)
     ctx.body = {
-      name: 'lxy'
+      attachments: [
+        {
+          color: '#0976fd',
+          blocks: [
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: "*Why is my new token expired?*\nI wants to know where you'd like to take the Paper Company investors to dinner tonight."
+              }
+            },
+            {
+              type: 'actions',
+              elements: [
+                {
+                  type: 'button',
+                  text: {
+                    type: 'plain_text',
+                    text: 'Share with channel',
+                    emoji: true
+                  },
+                  value: 'button_click'
+                },
+                {
+                  type: 'button',
+                  text: {
+                    type: 'plain_text',
+                    text: '👍',
+                    emoji: true
+                  },
+                  value: 'button_click'
+                },
+                {
+                  type: 'button',
+                  text: {
+                    type: 'plain_text',
+                    text: '👎',
+                    emoji: true
+                  },
+                  value: 'button_click'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          color: '#0976fd',
+          blocks: [
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: "*Why is my new token expired?*\nI wants to know where you'd like to take the Paper Company investors to dinner tonight."
+              }
+            },
+            {
+              type: 'actions',
+              elements: [
+                {
+                  type: 'button',
+                  text: {
+                    type: 'plain_text',
+                    text: 'Share with channel',
+                    emoji: true
+                  },
+                  value: 'button_click'
+                },
+                {
+                  type: 'button',
+                  text: {
+                    type: 'plain_text',
+                    text: '👍',
+                    emoji: true
+                  },
+                  value: 'button_click'
+                },
+                {
+                  type: 'button',
+                  text: {
+                    type: 'plain_text',
+                    text: '👎',
+                    emoji: true
+                  },
+                  value: 'button_click'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          color: '#0976fd',
+          blocks: [
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: "*Why is my new token expired?*\nI wants to know where you'd like to take the Paper Company investors to dinner tonight."
+              }
+            },
+            {
+              type: 'actions',
+              elements: [
+                {
+                  type: 'button',
+                  text: {
+                    type: 'plain_text',
+                    text: 'Share with channel',
+                    emoji: true
+                  },
+                  value: 'button_click'
+                },
+                {
+                  type: 'button',
+                  text: {
+                    type: 'plain_text',
+                    text: '👍',
+                    emoji: true
+                  },
+                  value: 'button_click'
+                },
+                {
+                  type: 'button',
+                  text: {
+                    type: 'plain_text',
+                    text: '👎',
+                    emoji: true
+                  },
+                  value: 'button_click'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          color: '#d8d1c9',
+          blocks: [
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: '*Not helpful?Try one of the options below*'
+              }
+            },
+            {
+              type: 'actions',
+              elements: [
+                {
+                  type: 'button',
+                  text: {
+                    type: 'plain_text',
+                    text: 'Ask a Channel',
+                    emoji: true
+                  },
+                  value: 'button_click'
+                },
+                {
+                  type: 'button',
+                  text: {
+                    type: 'plain_text',
+                    text: 'Manage FAQs',
+                    emoji: true
+                  },
+                  value: 'button_click'
+                }
+              ]
+            }
+          ]
+        }
+      ]
     }
     ctx.status = 200
   }
